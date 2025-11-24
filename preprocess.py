9fbdf299655487c3d4639010eb9625216665a6e0
HallowsYves <vyves246@gmail.com>
2025-11-24 14:51:02 -0800

    small changes

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,17 @@
 from datasets import load_dataset
 import pandas as pd 
 
-dataset = load_dataset("HallowsYves/CPSC481-data")
+try:
+    print("Loading Dataset...")
+    
+    dataset = load_dataset("HallowsYves/CPSC481-data")
 
-# Merge Data into two dataframes
-df_train = pd.concat([dataset["train"].to_pandas()])
-df_testing = pd.concat([dataset["test"].to_pandas()])
+    print("Converting to Dataframes...")
+    df_train = dataset['test'].to_pandas()
+    df_test = dataset['train'].to_pandas()
+
+    print("Column names: ")
+    print(df_train.columns.tolist())
+    print(f"\n Total Columns: {len(df_train.columns )}")
+except Exception as e:
+    print(f"Something Went Wrong {e}")
